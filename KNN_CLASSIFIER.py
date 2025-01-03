1cf441c05a73c2f4e99fc54fe59baa5d2f6218e6
shreyas om <shreyas.om@probo.in>
2025-01-03 14:23:53 +0530

    first commit

---
+++ KNN_CLASSIFIER.py
@@ -0,0 +1,34 @@
+import gensim.downloader as api
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
+def load_encoder():
+    word2vec_model = api.load("word2vec-google-news-300")
+    return word2vec_model
+
+def load_knn(path):
+    with open(path, 'rb') as handle:
+        knn = pickle.load(handle)
+    return knn
+
+def sentence_to_vec(sentence,word2vec_model):
+    words = sentence.split()
+    word_vecs = [word2vec_model[word] for word in words if word in word2vec_model]
+    if word_vecs:
+        return np.mean(word_vecs, axis=0)
+    else:
+        return np.zeros(word2vec_model.vector_size)
+
+def classify_knn(knn, sentence,word2vec_model):
+    sentence = sentence_to_vec(sentence,word2vec_model)
+    category = knn.predict([sentence])
+    return category[0]
+
+if __name__ == "__main__":
+    sentence = "TRXUSDT to be priced at 0.12344 USDT or more at 01:00 PM?. Tether's USDT that is issued on the TRON network. It is a technical standard token, and It works based on TRON's network\n"
+    print(sentence)
+    encoder = load_encoder()
+    knn = load_knn('tokenizer.pickle')
+    label = classify_knn(knn,sentence,encoder)
+    print(label)
